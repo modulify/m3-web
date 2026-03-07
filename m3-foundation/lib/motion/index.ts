d74729bead42cd76c8df7facef680ee1b5ece97d
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-03-07 14:16:35 +0400

    feat(m3-foundation): Added Motion Values And Surface Types

---
+++ m3-foundation/lib/motion/index.ts
@@ -0,0 +1,10 @@
+export type {
+  M3MotionDurationKey,
+  M3MotionEasingKey,
+} from './values'
+
+export {
+  m3MotionDurations,
+  m3MotionEasings,
+  m3MotionTimingDefaults,
+} from './values'
---
+++ m3-foundation/types/components/surface.d.ts
@@ -0,0 +1,26 @@
+export type Length = string | number
+
+export type Mode = 'standard' | 'modal'
+
+export type Variant =
+  | 'auto'
+  | 'surface'
+  | 'surface-dim'
+  | 'surface-bright'
+  | 'surface-container-lowest'
+  | 'surface-container-low'
+  | 'surface-container'
+  | 'surface-container-high'
+  | 'surface-container-highest'
+
+export type Anchor =
+  | 'none'
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'top'
+  | 'bottom'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom-start'
+  | 'bottom-end'
